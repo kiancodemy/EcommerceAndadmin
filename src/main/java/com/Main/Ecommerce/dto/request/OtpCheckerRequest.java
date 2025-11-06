8feaea192861d0fb5fc24baeec8e2e33044e0130
kianoosh <kiankheirani@gmail.com>
2025-11-06 14:01:46 +0330

    start-adding-authentication-testing

---
+++ src/main/java/com/Main/Ecommerce/dto/request/OtpCheckerRequest.java
@@ -0,0 +1,6 @@
+package com.Main.Ecommerce.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record OtpCheckerRequest(@NotBlank(message = "ایمیل نباید خالی باشد")String email, @NotBlank(message = "رمز یکبار مصرف نباید خالی باشد") String otp) {
+}
---
+++ src/main/java/com/Main/Ecommerce/dto/request/ResetPasswordRequest.java
@@ -0,0 +1,6 @@
+package com.Main.Ecommerce.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ResetPasswordRequest(@NotBlank(message = "ایمیل نباید خالی باشد")String email , @NotBlank(message = "پسورد نباید خالی باشد")String password , @NotBlank(message = "پسورد تاییدیه نباید خالی باشد")String confirmPassword ) {
+}
